e382f280cf5ee4fdb898b6d520717a4cb4044586
iagoaraujo <joseiago.p.a@gmail.com>
2016-10-13 00:00:54 -0300

    Calculo do bonus na heuristica

--- src/Maze.java
+++ src/Maze.java
@@ -11,15 +11,15 @@ public class Maze {
     private boolean[][] west;
     private boolean[][] visited;
     private boolean[][] initialVisited;
+    private boolean[][] bonus;
+    private boolean[][] trap;
+    private boolean[][] initialBonus;
+    private boolean[][] initialTrap;
     private boolean done = false;
     private int passos = 0;
     private List<int[]> listaAberta = new ArrayList<>();
     private List<int[]> caminho = new ArrayList<>();
-    private int posBonusX;
-    private int posBonusY;
-    private int posTrapX;
-    private int posTrapY;
-    private int extra = 0;
+    public boolean hasBonusOrTraps = false;
 
     public Maze(int n) {
         this.n = n;
@@ -31,33 +31,108 @@ public class Maze {
     }
     
     private void generateTrap() {
-    	Random generator = new Random();
-    	posBonusX = generator.nextInt(this.n);
-    	posBonusY = generator.nextInt(this.n);
-    	StdDraw.setPenColor(StdDraw.GREEN);
-        StdDraw.filledCircle(posBonusX + 0.5, posBonusY + 0.5, 0.25);
+    	if (initialTrap == null) {
+	        initialTrap  = new boolean[n+2][n+2];
+	        trap  = new boolean[n+2][n+2];
+	    	StdDraw.setPenColor(StdDraw.MAGENTA);
+	    	for (int i = 0; i < 5; i++) {
+	            int x = 1 + StdRandom.uniform(n-1);
+	            int y = 1 + StdRandom.uniform(n-1);
+	            StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
+	            initialTrap[x][y] = true;
+	        }
+    	} else {
+    		StdDraw.setPenColor(StdDraw.MAGENTA);
+			for (int x = 1; x <= n; x++) {
+	            for (int y = 1; y <= n; y++) {
+	                if (initialTrap[x][y]) StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
+	            }
+	        }
+		}
+    	trap = Arrays.copyOf(initialTrap, initialTrap.length);
 	}
 
 	private void generateBonus() {
-		Random generator = new Random();
-		posTrapX = generator.nextInt(this.n);
-		posTrapY = generator.nextInt(this.n);
-		StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledCircle(posTrapX + 0.5, posTrapY + 0.5, 0.25);
+		if (initialBonus == null) {
+			initialBonus  = new boolean[n+2][n+2];
+			StdDraw.setPenColor(StdDraw.GREEN);
+			for (int i = 0; i < 10; i++) {
+	            int x = 1 + StdRandom.uniform(n-1);
+	            int y = 1 + StdRandom.uniform(n-1);
+	            StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
+	            initialBonus[x][y] = true;
+	        }
+		} else {
+			StdDraw.setPenColor(StdDraw.GREEN);
+			for (int x = 1; x <= n; x++) {
+	            for (int y = 1; y <= n; y++) {
+	                if (initialBonus[x][y]) StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
+	            }
+	        }
+		}
+		bonus = Arrays.copyOf(initialBonus, initialBonus.length);
 	}
 	
-	private void checkForBonus(int x, int y){
-		if(posBonusX == x && posBonusY == y){
-			extra += 10;
+	private int checkForTrapOrBonusOnNorth(int x, int y){
+		int value = 10;
+		for (int i=y+1; i<n && value>0; i++) {
+			if (north[x][i] || bonus[x+1][i]) {
+				return value;
+			}
+			if (trap[x+1][i]) {
+				value = -5;
+				return value;
+			}
+			value--;
 		}
+		return value;
 	}
 	
-	private void checkForTrap(int x, int y){
-		if(posTrapX == x && posTrapY == y){
-			extra -= 10;
+	private int checkForTrapOrBonusOnSouth(int x, int y){
+		int value = 10;
+		for (int i=y-1; i>0 && value>0; i++) {
+			if (south[x][i] || bonus[x+1][i]) {
+				return value;
+			}
+			if (trap[x+1][i]) {
+				value -= 5;
+				return value;
+			}
+			value--;
 		}
+		return value;
 	}
-
+	
+	private int checkForTrapOrBonusOnWest(int x, int y){
+		int value = 10;
+		for (int i=x-1; i>0 && value>0; i--) {
+			if (west[i][y] || bonus[i][y+1]) {
+				return value;
+			}
+			if (trap[i][y+1]) {
+				value -= 5;
+				return value;
+			}
+			value--;
+		}
+		return value;
+	}
+	
+	private int checkForTrapOrBonusOnEast(int x, int y){
+		int value = 10;
+		for (int i=x+1; i<n && value>0; i--) {
+			if (east[i][y] || bonus[i][y+1]) {
+				return value;
+			}
+			if (trap[i][y+1]) {
+				value -= 5;
+				return value;
+			}
+			value--;
+		}
+		return value;
+	}
+	
 	public boolean[][] getEast() {
 		return east;
 	}
@@ -166,7 +241,6 @@ public class Maze {
     public void clear() {
     	this.visited = this.initialVisited.clone();
     	this.passos = 0;
-    	this.extra = 0;
     }
 
     // solve the maze using depth-first search
@@ -174,9 +248,7 @@ public class Maze {
         if (x == 0 || y == 0 || x == n+1 || y == n+1) return;
         if (done || visited[x][y]) return;
         visited[x][y] = true;
-        passos++;
-        checkForBonus(x, y);
-        checkForTrap(x, y);
+        walk(x, y);
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show();
@@ -185,13 +257,21 @@ public class Maze {
         // reached middle
         if (x == n && y == n) done = true;
 
-        if (!north[x][y]) solve(x, y + 1);
-        if (!east[x][y])  solve(x + 1, y);
-        if (!south[x][y]) solve(x, y - 1);
-        if (!west[x][y])  solve(x - 1, y);
+        if (!north[x][y]) {
+        	solve(x, y + 1);
+        }
+        if (!east[x][y]) {
+        	solve(x + 1, y);
+        }
+        if (!south[x][y]) { 
+        	solve(x, y - 1);
+        }
+        if (!west[x][y]) {
+        	solve(x - 1, y);
+        }
 
         if (done) return;
-        passos++;
+        walk(x, y);
         StdDraw.setPenColor(StdDraw.GRAY);
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show();
@@ -200,7 +280,7 @@ public class Maze {
 
     private void solveLista(int x, int y) {
         visited[x][y] = true;
-        passos++;
+        walk(x, y);
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show();
@@ -276,11 +356,11 @@ public class Maze {
     	if (x == 0 || y == 0 || x == n+1 || y == n+1) return;
         if (done || visited[x][y]) return;
         visited[x][y] = true;
-        passos++;
+        walk(x, y);
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show();
-        StdDraw.pause(50);
+        StdDraw.pause(30);
         // reached middle
         if (x == n && y == n) done = true;
         
@@ -291,18 +371,30 @@ public class Maze {
         if (!north[x][y]) {
         	norte = 1;
         	norte += n - y;
+        	if (hasBonusOrTraps) {
+        		norte += checkForTrapOrBonusOnNorth(x, y);
+        	}
         };
         if (!east[x][y]) {
         	leste = 1;
         	leste += n - x;
+        	if (hasBonusOrTraps) {
+        		leste += checkForTrapOrBonusOnEast(x, y);
+        	}
         };
         if (!south[x][y]) {
         	sul = -1;
         	sul += y - n;
+        	if (hasBonusOrTraps) {
+        		sul += checkForTrapOrBonusOnSouth(x, y);
+        	}
         };
         if (!west[x][y]) {
         	oeste = -1;
         	oeste += x - n;
+        	if (hasBonusOrTraps) {
+        		oeste += checkForTrapOrBonusOnWest(x, y);
+        	}
         };
         int lista[] = {norte, sul, leste, oeste};
         Arrays.sort(lista);
@@ -317,7 +409,7 @@ public class Maze {
         }
         
         if (done) return;
-        passos++;
+        walk(x, y);
         StdDraw.setPenColor(StdDraw.GRAY);
         StdDraw.filledCircle(x + 0.5, y + 0.5, 0.25);
         StdDraw.show();
@@ -361,6 +453,18 @@ public class Maze {
     public int getPassos() {
 		return passos;
 	}
+    
+    public void walk(int x, int y) {
+    	if (bonus[x][y] && hasBonusOrTraps) {
+        	bonus[x][y] = false;
+        	passos = passos - 10;
+        } else if (trap[x][y] && hasBonusOrTraps) {
+        	trap[x][y] = false;
+        	passos = passos + 5;
+        } else {
+        	passos++;
+        }
+    }
 
     // a test client
     public static void main(String[] args) {
@@ -368,30 +472,39 @@ public class Maze {
         Maze maze = new Maze(n);
         StdDraw.enableDoubleBuffering();
         maze.draw();
-        maze.generateBonus();
-        maze.generateTrap();
         StdDraw.pause(1000);
         maze.solve(1);
         System.out.println(maze.getPassos());
-        System.out.println(maze.extra);
         StdDraw.clear();
         StdDraw.show();
         maze.draw();
         maze.clear();
-        maze.generateBonus();
-        maze.generateTrap();
         maze.solve(0);
         System.out.println(maze.getPassos());
-        System.out.println(maze.extra);
+        StdDraw.clear();
+        StdDraw.show();
+        maze.draw();
+        maze.clear();
+        maze.solve(2);
+        System.out.println(maze.getPassos());
+        maze.hasBonusOrTraps = true;
         StdDraw.clear();
         StdDraw.show();
         maze.draw();
         maze.clear();
         maze.generateBonus();
         maze.generateTrap();
-        maze.solve(2);
+        maze.hasBonusOrTraps = true;
+        maze.solve(0);
         System.out.println(maze.getPassos());
-        System.out.println(maze.extra);
+        StdDraw.clear();
+        StdDraw.show();
+        maze.draw();
+        maze.clear();
+        maze.generateBonus();
+        maze.generateTrap();
+        maze.solve(1);
+        System.out.println(maze.getPassos());
     }
 
 }
